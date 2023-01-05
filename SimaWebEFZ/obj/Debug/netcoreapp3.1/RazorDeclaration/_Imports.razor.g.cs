b766ba9c0dbd6259c3a3abf8eb88efd0c1555db3
dan <dan@DESKTOP-9G0435A>
2023-01-04 18:25:27 -0600

    Feat:Add Entradas Buttons

--- SimaWebEFZ/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
+++ SimaWebEFZ/obj/Debug/netcoreapp3.1/RazorDeclaration/_Imports.razor.g.cs
@@ -13,63 +13,63 @@ namespace SimaWebEFZ
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 1 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 2 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 3 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 4 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 5 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 6 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 7 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 8 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using SimaWebEFZ;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 9 "C:\Users\dan\source\repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using SimaWebEFZ.Shared;
 
 #line default
