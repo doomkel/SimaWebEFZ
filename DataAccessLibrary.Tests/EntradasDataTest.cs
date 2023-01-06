6387feefe37eb6c3f382dbed9e479a80b497923f
doomkel <doomkel@hotmail.com>
2023-01-06 15:36:50 -0600

    Feat: Setup Unit Tests

---
+++ DataAccessLibrary.Tests/EntradasDataTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using Moq;
+using System.Data;
+using System.Collections.Generic;
+using DataAccessLibrary.Models;
+using System.Threading.Tasks;
+using Dapper;
+using System.Reflection;
+
+namespace DataAccessLibrary.Tests
+{
+    public class EntradasDataTest
+    {
+        [Fact]
+        public async void GetEntradas_Valid()
+        {
+            //arrange
+            Mock<ISqlDataAccess> connection = new Mock<ISqlDataAccess>();
+            var expected = new List<EntradasModel>{
+
+                new EntradasModel { Folio = 1, NombreRecibe = "Test1" },
+                new EntradasModel { Folio = 2, NombreRecibe = "Test2" }
+
+            };
+            connection.Setup(x => x.LoadData<EntradasModel, object>
+                (It.IsAny<string>(), It.IsAny<object>()))
+                .Returns(Task.FromResult(expected));
+
+
+            //act
+            var sut = new EntradasData(connection.Object);
+            List<EntradasModel> actual = await sut.GetEntradas();
+
+
+            //assert
+            Assert.True(actual.Count == 2);
+        }
+    }
+}
--- SimaWebEFZ/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Entradas.razor.g.cs
+++ SimaWebEFZ/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Entradas.razor.g.cs
@@ -13,77 +13,77 @@ namespace SimaWebEFZ.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 1 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 2 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 3 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 4 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 5 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 6 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 7 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 8 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using SimaWebEFZ;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
+#line 9 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\_Imports.razor"
 using SimaWebEFZ.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
+#line 3 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
 using DataAccessLibrary;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
+#line 4 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
 using DataAccessLibrary.Models;
 
 #line default
@@ -98,7 +98,7 @@ using DataAccessLibrary.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 83 "C:\Users\dan\Source\Repos\doomkel\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
+#line 83 "C:\Users\Daniel\source\repos\SimaWebEFZ\SimaWebEFZ\Pages\Entradas.razor"
        
     private List<EntradasModel> entradas;
 
